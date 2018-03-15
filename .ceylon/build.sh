3994c715febebbc6c8d0efc6df55e208711d0485
David Festal <dfestal@redhat.com>
2018-03-15 14:13:26 +0100

    Add Jolokia via `agent-bond`

--- .ceylon/build.sh
+++ .ceylon/build.sh
@@ -20,8 +20,14 @@ if [ "${ADD_REST_ENDPOINTS}" == "true" ]; then
     echo "Create the Wildfly Swarm application ..."
 
     ceylon plugin install ceylon.swarm/1.3.3 || true
-    ceylon swarm --dependencies=org.wildfly.swarm:jolokia:2018.3.3,org.wildfly.swarm:jaxrs:2018.3.3 --swarm-version=2018.3.3 --provided-module=javax.ws.rs:javax.ws.rs-api io.fabric8.tenant.che.migration.rest
+    ceylon swarm  --swarm-version=2018.3.3 --dependencies=org.wildfly.swarm:jaxrs:2018.3.3 --provided-module=javax.ws.rs:javax.ws.rs-api io.fabric8.tenant.che.migration.rest
     mv io.fabric8.tenant.che.migration.rest-*-swarm.jar io.fabric8.tenant.che.migration.rest.jar
+
+    mkdir -p agent-bond
+    curl --fail http://central.maven.org/maven2/io/fabric8/agent-bond-agent/1.2.0/agent-bond-agent-1.2.0.jar -o agent-bond/agent-bond.jar
+    curl --fail https://raw.githubusercontent.com/fabric8io-images/java/1.2.0/images/centos/openjdk8/jdk/java-container-options -o agent-bond/java-container-options
+    curl --fail https://raw.githubusercontent.com/fabric8io-images/java/1.2.0/images/centos/openjdk8/jdk/agent-bond-opts -o agent-bond/agent-bond-opts
+    chmod u+x agent-bond/*
 else
     echo
     echo "Wildfly Swarm application creation skipped..."
--- startRestEndpointsLocally.sh
+++ startRestEndpointsLocally.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-java -jar io.fabric8.tenant.che.migration.rest.jar
+script=$(readlink -f "$0")
+dir=$(dirname "$script")
+
+# Combine all java options
+get_java_options() {
+
+  # Normalize spaces (i.e. trim and elimate double spaces)
+  echo "${JAVA_OPTS} $($dir/agent-bond/agent-bond-opts) $($dir/agent-bond/java-container-options)" | awk '$1=$1'
+}
+
+export AB_DIR=$dir/agent-bond
+export AB_ENABLED=jolokia
+
+exec java $(get_java_options) -jar io.fabric8.tenant.che.migration.rest.jar
