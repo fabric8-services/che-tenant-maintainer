e39a64b72bbb7d8e9800a170d18e18e133c7023f
Roman Iuvshyn <riuvshyn@redhat.com>
2018-04-19 15:41:48 +0200

    Enable build on RHEL

--- buildAndPushToDocker.sh
+++ buildAndPushToDocker.sh
@@ -4,9 +4,10 @@ set -e
 REGISTRY=${REGISTRY:-"docker.io"}
 NAMESPACE=${NAMESPACE:-"dfestal"}
 ADD_REST_ENDPOINTS=${ADD_REST_ENDPOINTS:-false}
+DOCKERFILE=${DOCKERFILE:-"Dockerfile"}
 
 function tag_push() {
-  TARGET=$1
+  local TARGET=$1
   docker tag f8tenant-che-migration-deploy $TARGET
   docker push $TARGET
 }
@@ -15,7 +16,7 @@ if [ "$S2I_BUILD" == "true" ]; then
   s2i build -e ADD_REST_ENDPOINTS="${ADD_REST_ENDPOINTS}" -c . ceylon/s2i-ceylon:1.3.3-jre8 f8tenant-che-migration-deploy
 else
   ./buildLocally.sh
-  docker build -t f8tenant-che-migration-deploy .
+  docker build -t f8tenant-che-migration-deploy -f ${DOCKERFILE} .
 fi
 
 
