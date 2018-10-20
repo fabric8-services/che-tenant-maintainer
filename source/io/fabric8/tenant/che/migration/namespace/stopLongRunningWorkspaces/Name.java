4a580addc2b412600c1e3f8ffc95ba3208c4f1df
Angel Misevski <amisevsk@redhat.com>
2018-10-20 15:41:01 -0400

    Add new endpoint for stopping long-running workspaces
    Accessed via `stopLongRunningWorkspacesEndpoint`, see
    `stopLongRunningWorkspacesEndpoint/help` for options.
    
    Signed-off-by: Angel Misevsk <amisevsk@redhat.com>

---
+++ source/io/fabric8/tenant/che/migration/namespace/stopLongRunningWorkspaces/Name.java
@@ -0,0 +1,5 @@
+package io.fabric8.tenant.che.migration.namespace.stopLongRunningWorkspaces;
+
+public interface Name {
+  public static final String name = "stopLongRunningWorkspaces";
+}
---
+++ source/io/fabric8/tenant/che/migration/namespace/stopLongRunningWorkspaces/package.ceylon
@@ -0,0 +1,2 @@
+suppressWarnings("packageName")
+package io.fabric8.tenant.che.migration.namespace.stopLongRunningWorkspaces;
